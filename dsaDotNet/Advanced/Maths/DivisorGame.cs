55a13d2fe1c54035d06b77e8680ecbe29fee345b
Aurobinda Mohapatra <amlucky089@gmail.com>
2022-11-28 15:54:05 +1100

    Create DivisorGame.cs

---
+++ dsaDotNet/Advanced/Maths/DivisorGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace dsaDotNet.Advanced.Maths
+{
+	public class DivisorGame
+	{
+        public int solve(int A, int B, int C)
+        {
+            int gcd = GetGCD(B, C);
+            long b = B;
+            long lcm = (b * C) / gcd;
+
+            int ans = (int)(A / lcm);
+            return ans;
+
+        }
+        public int GetGCD(int a, int b)
+        {
+            if (b == 0) return a;
+            return GetGCD(b, a % b);
+        }
+    }
+}
+
